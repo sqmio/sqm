b129c16cb9039556623dcb4c580f6799a8e68604
def <def@MacBook-Pro-3.local>
2018-08-25 21:33:24 +0300

    add travis sonar addon

--- src/main/java/io/sqm/app/controller/HomeController.java
+++ src/main/java/io/sqm/app/controller/HomeController.java
@@ -2,14 +2,13 @@ package io.sqm.app.controller;
 
 import io.sqm.app.Example;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class HomeController {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping()
     public Example showHomePage() {
         Example example = new Example();
         example.setName("Hi...");
