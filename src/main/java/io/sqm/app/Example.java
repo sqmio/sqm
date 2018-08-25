a11bf5d9833d22f1431fbf1c3d71a35a853ac743
def <def@MacBook-Pro-3.local>
2018-08-25 19:11:49 +0300

    add home controller

---
+++ src/main/java/io/sqm/app/Example.java
@@ -0,0 +1,14 @@
+package io.sqm.app;
+
+public class Example {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/io/sqm/app/controller/HomeController.java
@@ -0,0 +1,19 @@
+package io.sqm.app.controller;
+
+import io.sqm.app.Example;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Example showHomePage() {
+        Example example = new Example();
+        example.setName("Hi");
+        return example;
+    }
+
+}
