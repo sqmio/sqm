b03356d38ab65a2b9feb0fea736b2aa4e934f4d8
def <def@MacBook-Pro-3.local>
2018-09-01 13:07:56 +0300

    add user entity

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,13 @@
-INSERT INTO claim (id, description) VALUES ('1','test1');
-INSERT INTO claim (id, description) VALUES ('2','test2');
-INSERT INTO claim (id, description) VALUES ('4','test4');
-INSERT INTO claim (id, description) VALUES ('5','test5');
-INSERT INTO claim (id, description) VALUES ('6','test6');
-INSERT INTO claim (id, description) VALUES ('7','test7');
-INSERT INTO claim (id, description) VALUES ('8','test8');
-INSERT INTO claim (id, description) VALUES ('9','test9');
-INSERT INTO claim (id, description) VALUES ('11','test11');
-INSERT INTO claim (id, description) VALUES ('12','test12');
+INSERT INTO users (id, login, full_name, first_name, last_name) VALUES ('1','test_login1', 'fullname1', 'firstname1', 'lastname1');
+INSERT INTO users (id, login, full_name, first_name, last_name) VALUES ('2','test_login2', 'fullname2', 'firstname2', 'lastname2');
+
+INSERT INTO claim (id, description, customer_id) VALUES ('1','test1', '2');
+INSERT INTO claim (id, description, customer_id) VALUES ('2','test2', '2');
+INSERT INTO claim (id, description, customer_id) VALUES ('4','test4', '1');
+INSERT INTO claim (id, description, customer_id) VALUES ('5','test5', '1');
+INSERT INTO claim (id, description, customer_id) VALUES ('6','test6', '2');
+INSERT INTO claim (id, description, customer_id) VALUES ('7','test7', '1');
+INSERT INTO claim (id, description, customer_id) VALUES ('8','test8', '2');
+INSERT INTO claim (id, description, customer_id) VALUES ('9','test9', '1');
+INSERT INTO claim (id, description, customer_id) VALUES ('11','test11', '1');
+INSERT INTO claim (id, description, customer_id) VALUES ('12','test12', '1');
